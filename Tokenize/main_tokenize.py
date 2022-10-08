26dbef3169e3f25218ffa2adffed0f588f285ebd
HoangHaGiang2001 <92769525+hoanggumball@users.noreply.github.com>
2022-10-08 23:04:03 +0700

    tokenize and tf

--- Tokenize/main_tokenize.py
+++ Tokenize/main_tokenize.py
@@ -1,7 +1,9 @@
 import underthesea
+import math
 import pandas as pd
 import nltk
 
+# dic = {1: "a new car used car car review", 2: "a friend in need is a friend indeed"}
 dic = {}
 
 for i in range(45, 77):
@@ -14,6 +16,7 @@ for i in range(45, 77):
     finally:
         article.close()
 
+
 # calculate tf
 corpus = {}
 docFreq = []
@@ -22,6 +25,7 @@ for article in dic.values():
     data = underthesea.word_tokenize(data)
     # Not handle blacklist
     freq = {}
+    # iterate through the doc to create a dic of tokens and their tf
     for token in data:
         if token in freq.keys():
             freq[token] += 1
@@ -31,7 +35,20 @@ for article in dic.values():
         freq[key] /= len(data)
     docFreq.append(freq)
 
-print(len(docFreq))
+# calculate tf-idf
+for record in docFreq:
+    for token in record:
+        count = 0
+        for dictionary in docFreq:
+            if token in dictionary:
+                count += 1
+        idf = math.log10(len(docFreq)/count)
+        record[token] *= idf
+
+# print the record
+for record in docFreq:
+    print(record)
+    print('\n')
 # convert to lower case
 # data = dic['45'].lower()
 # data = underthesea.word_tokenize(data)
