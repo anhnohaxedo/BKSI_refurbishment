03534b5e5acf49c06cda9f57eccc4d568082f2e6
anhnohaxedo <89699656+anhnohaxedo@users.noreply.github.com>
2022-10-22 14:13:47 +0700

    Fix some bugs

--- scrapping/ussh_scrapping.py
+++ scrapping/ussh_scrapping.py
@@ -57,19 +57,21 @@ def get_article(name, link):
     body = driver.find_element(By.CLASS_NAME, 'section-body')
     wrapper = body.find_elements(By.CLASS_NAME, 'wraper')[1].find_element(By.CLASS_NAME, 'panel-body')
     header = wrapper.find_element(By.CSS_SELECTOR, 'h1')
-    print(header)
     clearfix = wrapper.find_element(By.CLASS_NAME, 'clearfix')
-    new_body = wrapper.find_element(By.ID, 'news-bodyhtml')
+    news_body = wrapper.find_element(By.ID, 'news-bodyhtml').find_elements(By.CSS_SELECTOR, '*')
     try:
         author = wrapper.find_element(By.CSS_SELECTOR, 'p').text
     except:
         author = ''
     file = open('D:/project/BKSI/BKSI_refurbishment/scrapping/ussh/' + name + '.txt', 'w', newline='', encoding='utf-8')
-    print(header.text)
     file.write(clearfix.text)
-    for element in new_body.find_elements(By.CSS_SELECTOR, 'div'):
-        if element.text != '':
+    for element in news_body:
+        print(element.tag_name)
+        if element.tag_name == 'p':
             file.write(element.text)
+        elif (element.text != ''):
+            file.write(element.text)
+
     file.write(author)
     file.close()
     driver.quit()
@@ -79,3 +81,4 @@ news = get_news(categories=categories)
 
 for i in range(len(news)):
     get_article('new_' + str(i), news[i])
+
