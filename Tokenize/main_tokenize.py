bff127b71fd6e9fbe4bbc3c5207a0db0c3b2e81a
HoangHaGiang2001 <92769525+hoanggumball@users.noreply.github.com>
2022-12-12 19:23:59 +0700

    update evaluating code (not quite complete)

--- Tokenize/main_tokenize.py
+++ Tokenize/main_tokenize.py
@@ -11,13 +11,22 @@ from sklearn.datasets import load_iris
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
-
-# What's the tf-idf variant you want to run? original = 0; sublinear_tf_idf = 1; maximum_tf = 2
-tf_idf_Variant = 0
+# What's the tf-variant you want to run? original = 0; sublinear_tf_idf = 1; maximum_tf = 2; another sublinear tf = 3
+tf_Variant = 3
+# What's the idf-variant you want to run? original = 0; idf_smooth = 1; idf_max = 2; probabilistic_idf = 3
+idf_Variant = 1
 # the normalization coefficient had you chosen maximum_tf = 2
 normalize_a = 0.5
 # changing this to use either dataset 1 or 2, choose 0 to not use test dataset at all
 testing_dataset = 1
+
+
+def presition_calc(truepos, falsepos):
+    return truepos / (truepos + falsepos)
+
+
+def recall_calc(truepos, falseneg):
+    return truepos / (truepos + falseneg)
 
 
 dataset = []
@@ -25,6 +34,20 @@ dataset1 = [
     "Tôi yêu Google. Tôi ghét Bing. Google là số một",
     "Tôi yêu Pushkin. Pushkin là số một"
 ]
+dataset1 = [
+    "8 địa chỉ khám sức khỏe tổng quát tốt nhất ở Hà Nội",
+    "Top 8 Các Bệnh Viện Uy Tín Tốt Nhất Ở Hà Nội",
+    "Điểm danh Top 7 bệnh viện tốt ở Hà Nội đáng tin cậy nhất",
+    "Danh sách các phòng khám đa khoa tốt uy tín nhất ở tại Hà Nội",
+    "3 bệnh viện công khám sức khỏe tổng quát tốt ở Hà Nội",
+    "20 Quán Ăn Ngon Nhất Hà Nội Thuần Vị Bắc Ăn Xong Vẫn Thèm",
+    "Bỏ túi top 30 quán ăn ngon Hà Nội bạn nhất định phải thử",
+    "10 bệnh viện chất lượng nhất TP HCM",
+    "8 bệnh viện, phòng khám Thần kinh uy tín tại TP.HCM",
+    "Top 10 bệnh viên tư nhân TPHCM uy tín nhất",
+    "Đăng ký khám bệnh online tại Hà Nội giúp tiết kiệm thời gian"
+]
+
 dataset2 = [
     "Tôi yêu Google",
     "Tôi ghét Bing",
@@ -34,6 +57,17 @@ dataset2 = [
 ]
 dic2 = {1: "Tôi yêu Google", 2: "Tôi ghét Bing", 3: "Google là số một", 4: "Tôi yêu Puskin", 5: "Puskin là số một"}
 dic1 = {1: "Tôi yêu Google. Tôi ghét Bing. Google là số một", 2: "Tôi yêu Pushkin. Pushkin là số một"}
+dic1 = {1: "8 địa chỉ khám sức khỏe tổng quát tốt ở Hà Nội",
+    2: "Top 8 Các Bệnh Viện Uy Tín Tốt Nhất Ở Hà Nội",
+    3: "Điểm danh Top 7 bệnh viện ở Hà Nội đáng tin cậy nhất",
+    4: "Danh sách các phòng khám đa khoa uy tín nhất ở tại Hà Nội",
+    5: "3 bệnh viện công khám sức khỏe tổng quát tốt ở Hà Nội",
+    6: "20 Quán Ăn Ngon Nhất Hà Nội Thuần Vị Bắc Ăn Xong Vẫn Thèm",
+    7: "Bỏ túi top 30 quán ăn ngon nhất Hà Nội bạn nhất định phải thử",
+    8: "10 bệnh viện chất lượng tốt nhất TP HCM",
+    9: "8 bệnh viện, phòng khám Thần kinh uy tín nhất tại TP.HCM",
+    10: "Top 10 bệnh viên tư nhân TPHCM uy tín nhất",
+    11: "Đăng ký khám bệnh online tại Hà Nội giúp tiết kiệm thời gian"}
 dic = {}
 
 if testing_dataset == 1:
@@ -84,20 +118,17 @@ for dic_key in dic.keys():
         if max_term < freq[token]:
             max_term = freq[token]
     for key in freq.keys():
-        if tf_idf_Variant == 0:  # Normal tf-idf
+        if tf_Variant == 0:  # Normal tf-idf
             freq[key] /= len(data)
-        elif tf_idf_Variant == 1:  # Sublinear tf-idf
+        elif tf_Variant == 1:  # Sublinear tf-idf
             if (freq[key] / len(data)) > 0:
-                print(key)
-                print(freq[key] / len(data))
-                print(math.log10(freq[key]/len(data)))
                 freq[key] = 1 + math.log10(freq[key]/len(data))
-                print(freq[key])
             else:
                 freq[key] = 0
-        elif tf_idf_Variant == 2:  # Maximum tf normalization
+        elif tf_Variant == 2:  # Maximum tf normalization
             freq[key] = normalize_a + (1 - normalize_a)*(freq[key]/max_term)
-
+        elif tf_Variant == 3:  # Maximum tf normalization
+            freq[key] = math.log10(freq[key]/len(data) + 1)
     docFreq[dic_key] = freq
 
 # calculate tf-idf
@@ -107,7 +138,14 @@ for record in docFreq.values():
         for dictionary in docFreq.values():
             if token in dictionary:
                 count += 1
-        idf = math.log10((len(docFreq))/(count+1)) + 1
+        if idf_Variant == 0:  # Normal idf
+            idf = math.log10(len(docFreq) / count)
+        elif idf_Variant == 1:  # idf
+            idf = math.log10((len(docFreq)) / (count + 1)) + 1
+        # elif idf_Variant == 2:
+        #         # do smth
+        # elif idf_Variant == 3:
+        #         # do smth
         record[token] *= idf
 
 
@@ -127,7 +165,8 @@ for key in docFreq.keys():
 # data = underthesea.word_tokenize(data)
 # print(data)
 
-data_query = "car is indeed"
+data_query = "bệnh viện tốt nhất Hà Nội"
+# data_query = "car is indeed"
 
 # try:
 #     query = open("D:/BKSI_refurbishment/scrapping/articles/query.txt", "r", encoding='utf-8')
@@ -152,34 +191,54 @@ for token in data_query_list:
             query_weights[key] = 0
         if token in docFreq[key]:
             # print(token + " " + str(docFreq[key][token]))
-            query_weights[key] += docFreq[key][token] + 1
+            query_weights[key] += docFreq[key][token]
 
 sorted_weights = {k: v for k, v in sorted(query_weights.items(), key=lambda item: item[1], reverse=True)}
 
-# print(sorted_weights)
+print(sorted_weights)
+precisionList = []
+recallList = []
 
-print("scikit learn result:")
-tfIdfTransformer = TfidfTransformer(use_idf=True)
-countVectorizer = CountVectorizer()
-wordCount = countVectorizer.fit_transform(dataset)
-newTfIdf = tfIdfTransformer.fit_transform(wordCount)
-df = pd.DataFrame(newTfIdf[0].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
-df = df.sort_values('TF-IDF', ascending=False)
-print(df)
-df = pd.DataFrame(newTfIdf[1].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
-df = df.sort_values('TF-IDF', ascending=False)
-print("---")
-print(df)
-df = pd.DataFrame(newTfIdf[2].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
-df = df.sort_values('TF-IDF', ascending=False)
-print("---")
-print(df)
-df = pd.DataFrame(newTfIdf[3].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
-df = df.sort_values('TF-IDF', ascending=False)
-print("---")
-print(df)
-df = pd.DataFrame(newTfIdf[4].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
-df = df.sort_values('TF-IDF', ascending=False)
-print("---")
-print(df)
+counter = 0
+relevant_retrieve_counter = 0
+for elem in sorted_weights.keys():
+    counter += 1
+    if int(elem) <= 5:
+        relevant_retrieve_counter += 1
+        precisionList.append(presition_calc(relevant_retrieve_counter, counter - relevant_retrieve_counter))
+        recallList.append(presition_calc(relevant_retrieve_counter, 5 - relevant_retrieve_counter))
+    else:
+        precisionList.append(presition_calc(relevant_retrieve_counter, counter - relevant_retrieve_counter))
+        recallList.append(presition_calc(relevant_retrieve_counter, 5 - relevant_retrieve_counter))
+
+print(precisionList)
+print(recallList)
+
+
+
+
+# print("scikit learn result:")
+# tfIdfTransformer = TfidfTransformer(use_idf=True)
+# countVectorizer = CountVectorizer()
+# wordCount = countVectorizer.fit_transform(dataset)
+# newTfIdf = tfIdfTransformer.fit_transform(wordCount)
+# df = pd.DataFrame(newTfIdf[0].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
+# df = df.sort_values('TF-IDF', ascending=False)
+# print(df)
+# df = pd.DataFrame(newTfIdf[1].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
+# df = df.sort_values('TF-IDF', ascending=False)
+# print("---")
+# print(df)
+# df = pd.DataFrame(newTfIdf[2].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
+# df = df.sort_values('TF-IDF', ascending=False)
+# print("---")
+# print(df)
+# df = pd.DataFrame(newTfIdf[3].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
+# df = df.sort_values('TF-IDF', ascending=False)
+# print("---")
+# print(df)
+# df = pd.DataFrame(newTfIdf[4].T.todense(), index=countVectorizer.get_feature_names(), columns=["TF-IDF"])
+# df = df.sort_values('TF-IDF', ascending=False)
+# print("---")
+# print(df)
 
